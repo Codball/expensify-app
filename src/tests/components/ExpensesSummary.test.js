32b32b7cac9d0f5ee150fba343732fcbeb54a201
Codball <nathanccaldwell@gmail.com>
2018-07-24 12:25:05 -0400

    Add ExpenseSummary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
+
+test('should correctly render expenses summary with one expense', () => {
+    const wrapper = shallow();
+});
+
+test('should correctly render expenses summary with multiple expenses', () => {
+
+});
